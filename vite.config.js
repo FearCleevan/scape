816e58a074b128a2d1e893b8ed013cc701f96e0c
FearCleevan <jonathan.mauring17@gmail.com>
2025-11-12 03:49:59 +0800

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', // Ensure this is set to root
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
-      }
-    }
+    historyApiFallback: true,
   }
-});
+})
